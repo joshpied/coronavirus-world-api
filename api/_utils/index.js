cae768d7dcf410bf966fa82276322aff80969c36
Josh Piedimonte <joshpied@gmail.com>
2020-03-28 13:49:39 -0400

    Moved dates to their own key in countryObj, made util function for formatting countryObj

--- api/_utils/index.js
+++ api/_utils/index.js
@@ -1,18 +1,61 @@
 export const convertTotalsToNewCases = countriesObj => {
   for (let country in countriesObj) {
     let previousDayTotal = 'init';
-    for (let dateKey in countriesObj[country]) {
-      if (dateKey !== 'coordinates') {
-        if (previousDayTotal === 'init') {
-          previousDayTotal = countriesObj[country][dateKey];
-        } else {
-          const currentDayTotal = countriesObj[country][dateKey];
-          countriesObj[country][dateKey] = currentDayTotal - previousDayTotal;
-          previousDayTotal = currentDayTotal;
-        }
+    for (let dateKey in countriesObj[country]['dates']) {
+      if (previousDayTotal === 'init') {
+        previousDayTotal = countriesObj[country]['dates'][dateKey];
+      } else {
+        const currentDayTotal = countriesObj[country]['dates'][dateKey];
+        countriesObj[country]['dates'][dateKey] = currentDayTotal - previousDayTotal;
+        previousDayTotal = currentDayTotal;
       }
     }
   }
 
   return countriesObj;
+};
+
+export const formatToCountryObj = stats => {
+  const formattedStatsObj = {};
+
+  for (let i in stats) {
+    const countryName = stats[i]['Country/Region'];
+
+    // countries with regional data: increment the count
+    if (countryName in formattedStatsObj) {
+      for (let dateKey in formattedStatsObj[countryName]['dates']) {
+        formattedStatsObj[countryName]['dates'][dateKey] += parseInt(
+          stats[i][dateKey]
+        );
+      }
+      // countries added to obj here (countries without regional data will only use this part)
+    } else {
+      formattedStatsObj[countryName] = { ...stats[i] }; // init new country
+      // add geo coordinates property to new country object
+      const coordinates = {
+        latitude: parseFloat(formattedStatsObj[countryName]['Lat']),
+        longitude: parseFloat(formattedStatsObj[countryName]['Long'])
+      };
+      formattedStatsObj[countryName]['coordinates'] = coordinates;
+      // remove data no longer needed
+      delete formattedStatsObj[countryName]['Country/Region'];
+      delete formattedStatsObj[countryName]['Province/State'];
+      delete formattedStatsObj[countryName]['Lat'];
+      delete formattedStatsObj[countryName]['Long'];
+      formattedStatsObj[countryName]['dates'] = {};
+      // put every 'date': 'amount' pair in a new dates property and make them integers
+      for (let dateKey in stats[i]) {
+        if (
+          !['Lat', 'Long', 'Province/State', 'Country/Region'].includes(dateKey)
+        ) {
+          formattedStatsObj[countryName]['dates'][dateKey] = parseInt(
+            stats[i][dateKey]
+          );
+          delete formattedStatsObj[countryName][dateKey];
+        }
+      }
+    }
+  }
+
+  return formattedStatsObj;
 };
--- api/time-series/deceased/new.js
+++ api/time-series/deceased/new.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import {csv} from 'csvtojson';
-import { convertTotalsToNewCases } from '../../_utils';
+import { csv } from 'csvtojson';
+import { convertTotalsToNewCases, formatToCountryObj } from '../../_utils';
 
 module.exports = async (req, res) => {
   const url = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv`;
@@ -8,39 +8,7 @@ module.exports = async (req, res) => {
     const response = await axios.get(url);
     const statsCsvString = response.data;
     const stats = await csv().fromString(statsCsvString);
-    const formattedStatsObj = {};
-
-    for (let i in stats) {
-      const countryName = stats[i]['Country/Region'];
-
-      if (countryName in formattedStatsObj) {
-        for (let dateKey in formattedStatsObj[countryName]) {
-          if (dateKey !== 'coordinates') {
-            formattedStatsObj[countryName][dateKey] += parseInt(
-              stats[i][dateKey]
-            );
-          }
-        }
-      } else {
-        formattedStatsObj[countryName] = stats[i];
-        const coordinates = {
-          latitude: parseFloat(formattedStatsObj[countryName]['Lat']),
-          longitude: parseFloat(formattedStatsObj[countryName]['Long'])
-        };
-        formattedStatsObj[countryName]['coordinates'] = coordinates;
-        delete formattedStatsObj[countryName]['Country/Region'];
-        delete formattedStatsObj[countryName]['Province/State'];
-        delete formattedStatsObj[countryName]['Lat'];
-        delete formattedStatsObj[countryName]['Long'];
-        for (let dateKey in formattedStatsObj[countryName]) {
-          if (dateKey !== 'coordinates')
-            formattedStatsObj[countryName][dateKey] = parseInt(
-              formattedStatsObj[countryName][dateKey]
-            );
-        }
-      }
-    }
-
+    const formattedStatsObj = formatToCountryObj(stats);
     const newDeceasedByCountry = convertTotalsToNewCases(formattedStatsObj);
 
     res.status(200).json({
